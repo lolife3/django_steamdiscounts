21c648a71db057ce6f15302932e88829a834c885
lolife3 <antek442@op.pl>
2023-06-20 22:18:00 +0200

    update

--- djangoapp/myapi/serializers.py
+++ djangoapp/myapi/serializers.py
@@ -4,11 +4,11 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ("__all__")
+        fields = ["url", "username", "email", "groups"]
         
         
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ("__all__")
+        fields = ["url", "name"]
