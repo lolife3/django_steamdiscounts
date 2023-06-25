88df1e9ba000d75e7e94ef61322352ff7d1ffa02
lolife3 <antek442@op.pl>
2023-06-25 21:46:14 +0200

    added cbv

--- djangoapp/myapi/views.py
+++ djangoapp/myapi/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import  UserSerializer, DiscountsSerializer
 from main.models import Discounts
 
@@ -9,6 +9,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_class = [permissions.IsAuthenticated]
+    
 
 
 class DiscountsViewSet(viewsets.ModelViewSet):
@@ -16,3 +17,17 @@ class DiscountsViewSet(viewsets.ModelViewSet):
     serializer_class = DiscountsSerializer
     permission_class = [permissions.IsAuthenticated]
 
+
+
+class _DiscountsViewSet(APIView):
+    def get(self, request, format=None):
+        queryset = Discounts.objects.all()
+        serializer = DiscountsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DiscountsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
